4df832ac88fabf235ced74957f531fda628e1183
Seshouan <session.mwamufiya@gmail.com>
2025-01-10 18:09:12 -0800

    built the chatbot GUI and actively troubleshooting errors when trying to train the model

---
+++ gui_chatbot.py
@@ -0,0 +1,104 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import numpy as np
+
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern (split words into an array)
+    sentence_words = nltk.word_tokenize(sentence)
+    # lemmatize each word (reduce to base form)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array of numbers (1 if word in sentence, else 0)
+def bag_of_words(sentence, words, show_details=True):
+    # tokenize patterns
+    sentence_words = clean_up_sentence(sentence)
+    # obtain the bag of words (the vocabulary matrix)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, word in enumerate(words):
+            if word == s:
+                bag[i] = 1
+                if show_details:
+                    print(f'bag contains: {word}')
+    return(np.array(bag))
+
+def predict_class(sentence):
+    # filter below threshold predictions
+    p = bag_of_words(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sort strength probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if (i['tag']==tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+# create the tkinter GUI
+import tkinter
+from tkinter import *
+
+def send():
+    msg = EntryBox.get('1.0', 'end-1c').strip()
+    EntryBox.delete('0.0', END)
+
+    if msg != '':
+        ChatBox.config(state=NORMAL)
+        ChatBox.insert(END, 'You: ' + msg + '\n\n')
+        ChatBox.config(foreground='#446665', font=('Verdana', 12))
+        
+        ints = predict_class(msg)
+        res = getResponse(ints, intents)
+
+        ChatBox.insert(END, 'Bot: ' + res + '\n\n')
+
+        ChatBox.config(state = DISABLED)
+        ChatBox.yview(END)
+
+root = Tk()
+root.title('Chatbot')
+root.geometry(width=False, height=False)
+
+# create chat window
+ChatBox = Text(root, bd=0, bg='white', height='8', width='50', font='Arial',)
+
+ChatBox.config(state=DISABLED)
+
+# bind scrollbar to chat window
+scrollbar = Scrollbar(root, command=ChatBox.yview, cursor='heart')
+ChatBox['yscrollcommand'] = scrollbar.set
+
+# create a send message button
+SendButton = Button(root, font=('Verdana', 12, 'bold'), text='Send', width='12', height='5', bd=0, bg='#f9a602', activebackground='#3c9d9b', fg='#000000', command=send)
+
+# create an input box
+EntryBox = Text(root, bd=0, bg='white', width='29', height='5', font='Arial')
+#EntryBox.bind('<Return>', send)
+
+# arrange all components
+scrollbar.place(x=376, y=6, height=386)
+ChatBox.place(x=6, y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+root.mainloop()
---
+++ train_chatbot.py
@@ -0,0 +1,101 @@
+# import required modules
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+import nltk
+from nltk.stem import WordNetLemmatizer
+import json
+import pickle
+
+# read in a json file
+lemmatizer = WordNetLemmatizer()
+intents_file = open('intents.json').read()
+intents = json.loads(intents_file)
+
+# preprocessing: tokenizer
+words = []
+classes = []
+documents = []
+ignore_letters = list('!?,.')
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # tokenize each word
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
+        # add documents to the corpus
+        documents.append((word, intent['tag']))
+        # add to the classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+print(documents)
+
+# preprocessing: lemmatizer
+
+# lemmatize and lowercase each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+words = sorted(list(set(words)))
+# sort classes
+classes = sorted(list(set(classes)))
+# print number of documents, which combine patterns and intents
+print(len(documents), 'documents')
+# print classes as intents
+print(len(classes), 'classes', classes)
+# print all words in the vocabulary
+print(len(words), 'unique lemmatized words', words)
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# convert input patterns to numbers
+
+# create the training data
+training = []
+# create output array
+output_empty = [0] * len(classes)
+# use a bag of words for every sentence as the training set
+for doc in documents:
+    # init a bag of words
+    bag = []
+    # get the list of tokenized words for the pattern
+    word_pattern = doc[0]
+    # lemmatize each word
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    # flag words that appear in the current pattern as 1s the rest as 0s
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    # output 1s for the current tag and 0s otherwise
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    training.append([bag, output_row])
+# shuffle the training set and turn it to a numpy array
+random.shuffle(training)
+training = np.array(training)
+# create training inputs (patterns) and label (intent) sets
+train_X = list(training[:,0])
+train_y = list(training[:,1])
+print('created training data')
+
+# train the model with 3 layers deep, using dropout layers and the SGD optimizer; train 200 epochs
+
+# define the deep neural net
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_X[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# compile the model using SGD with Nesterov accelerated gradient
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# train and save the model
+hist = model.fit(np.array(train_X), np.array(train.y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print('created model')
